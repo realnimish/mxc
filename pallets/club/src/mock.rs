1b4491ed6d9dd9679803f4311db45726f14f6182
realnimish <realnimish@gmail.com>
2022-07-25 09:52:59 +0530

    Added pallet club

--- pallets/club/src/mock.rs
+++ pallets/club/src/mock.rs
@@ -1,4 +1,4 @@
-use crate as pallet_template;
+use crate as pallet_club;
 use frame_support::traits::{ConstU16, ConstU64};
 use frame_system as system;
 use sp_core::H256;
@@ -18,7 +18,7 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		ClubModule: pallet_club::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
@@ -49,8 +49,13 @@ impl system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
-impl pallet_template::Config for Test {
+parameter_types! {
+	pub const MaxLength: u32 = 50;
+}
+
+impl pallet_club::Config for Test {
 	type Event = Event;
+	type MaxLength = MaxLength;
 }
 
 // Build genesis storage according to the mock runtime.
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -43,8 +43,7 @@ use pallet_transaction_payment::CurrencyAdapter;
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+pub use pallet_club;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -262,9 +261,13 @@ impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+parameter_types! {
+	pub const MaxLength: u32 = 50;
+}
+
+impl pallet_club::Config for Runtime {
 	type Event = Event;
+	type MaxLength = MaxLength;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
@@ -282,8 +285,7 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		ClubModule: pallet_club,
 	}
 );
 
@@ -328,7 +330,6 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
 	);
 }
 
